83f1d63ba572fc56c25906b4cdd2e42194dac46a
Miguel Teles <miguelteles2002@hotmail.com>
2022-09-04 18:53:09 -0300

    Service layer, component registration

---
+++ src/main/java/com/example/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.example.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.course.entities.User;
+import com.example.course.repositories.UserRepository;
+
+//precisa registrar essa classe no Spring pra q o autowired dela funcione
+//@Component
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
